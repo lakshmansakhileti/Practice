abf628551bb210d321a2b3f00a1e0db6b314b24a
Lakshman <lakshmansakhileti@gmail.com>
2021-02-05 12:48:50 +0530

    Program to check given string is A-good or not

---
+++ src/com/divideandconquer/A_Good_String.java
@@ -0,0 +1,57 @@
+package com.divideandconquer;
+
+public class A_Good_String {
+    public static void main(String[] args) {
+        String input = "ccde";
+        char c = 'c';
+       Boolean result =  isFirstHalfGoodString(c,input) ||
+        isSecondHalfGoodString(input.charAt(input.length()/2),input);
+
+        System.out.println("Is input  string  "+input +" is "+c+"- good string? "+ result);
+    }
+
+    public static boolean isAgoodString(char c, String input) {
+        if(input.length() == 1){
+            return c == input.charAt(0);
+        }
+        int start = 0;
+        int totalChars = input.length();
+       return isCString(c, input.substring(start, totalChars/2)) &&
+               isAgoodString((char)((int)c+1), input.substring(totalChars/2, totalChars));
+    }
+
+    public static boolean isFirstHalfGoodString(char c, String input) {
+        if(input.length() == 1){
+            return c == input.charAt(0);
+        }
+        int start = 0;
+        int totalChars = input.length();
+        return isCString(c, input.substring(start, totalChars/2)) &&
+                isAgoodString((char)((int)c+1), input.substring(totalChars/2, totalChars));
+    }
+
+    public static boolean isSecondHalfGoodString(char c, String input) {
+        if(input.length() == 1){
+            return c == input.charAt(0);
+        }
+        int start = 0;
+        int totalChars = input.length();
+        return isCString(c, input.substring(totalChars/2, totalChars)) &&
+                isAgoodString((char)((int)c+1), input.substring(start, totalChars/2));
+    }
+
+
+    public static boolean isCString(char c, String input) {
+        if(input.length() == 1){
+            return c == input.charAt(0);
+        }
+        int start =0;
+        int end=input.length()-1;
+        for(int i=start, j=end; i<j; i++, j--) {
+            if(c != input.charAt(i) || c != input.charAt(j) ) {
+                return false;
+            }
+        }
+        return  true;
+    }
+}
